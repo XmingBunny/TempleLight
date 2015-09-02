6d1e540ac5303c8a645aa5052d67a60cf3eb6743
SiuMing <2457944792@qq.com>
2015-09-02 13:49:27 +0800

    Add SignUpUI

--- Assets/Scripts/Manager/SceneManger.cs
+++ Assets/Scripts/Manager/SceneManger.cs
@@ -4,7 +4,14 @@ using System.Collections;
 public class SceneManger : MonoBehaviour 
 {
     public GameObject Particles;
+    public GameObject SignUpPanel;
+    public GameObject SignInPanel;
+
+    
     protected AudioSource audio;
+
+    private bool isSignUp = false;
+    private float time = 0.5f;
 
     void Awake()
     {
@@ -19,5 +26,24 @@ public class SceneManger : MonoBehaviour
     public void SetParticles(bool isOn)
     {
         Particles.SetActive(isOn);
+    }
+
+    public void PlaneMove()
+    {
+       
+        if (isSignUp)
+        {
+            iTween.MoveTo(SignUpPanel, SignUpPanel.transform.position + new Vector3(5330, 0, 0), time);
+            iTween.MoveTo(SignInPanel, SignInPanel.transform.position + new Vector3(5330, 0, 0), time);
+
+            isSignUp = !isSignUp;
+        }
+        else
+        {
+            iTween.MoveTo(SignUpPanel, SignUpPanel.transform.position - new Vector3(5330, 0, 0), time);
+            iTween.MoveTo(SignInPanel, SignInPanel.transform.position - new Vector3(5330, 0, 0), time);
+
+            isSignUp = !isSignUp;
+        }
     }
 }
