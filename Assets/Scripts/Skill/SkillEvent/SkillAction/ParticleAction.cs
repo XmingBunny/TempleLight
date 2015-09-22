d6d4dff482eb16a6eddb609c1b4ed4dbf1e4fa3c
SiuMing <2457944792@qq.com>
2015-09-22 11:12:01 +0800

    粒子效果的相对坐标转化为世界坐标

--- Assets/Scripts/Skill/SkillEvent/SkillAction/ParticleAction.cs
+++ Assets/Scripts/Skill/SkillEvent/SkillAction/ParticleAction.cs
@@ -13,13 +13,24 @@ public class ParticleAction : SkillAction
     public override void Execute()
     {
         gameObject = (GameObject)GameObject.Instantiate(Particle);
-        gameObject.transform.parent = _Skill.unitController.transform;
-        gameObject.transform.localPosition = Position;
-        gameObject.transform.rotation = _Skill.unitController.transform.rotation;
-        //gameObject.transform.parent = null;
+        gameObject.transform.position = CoordinateTransitionToWorld(_Skill.unitController.transform, Position);
+
         particleController = gameObject.GetComponent<ParticleController>();
         particleController.ExistTime = ExistTime;
         particleController.StartTimer();
+    }
+
+    //将相对于unit的坐标转化为世界坐标
+    Vector3 CoordinateTransitionToWorld(Transform parent, Vector3 RelativePosition)
+    {
+        float Radian = parent.eulerAngles.y / 180f * Mathf.PI;
+        float x = RelativePosition.x * Mathf.Cos(Radian) + RelativePosition.z * Mathf.Sin(Radian);
+        float y = RelativePosition.y;
+        float z = -RelativePosition.x * Mathf.Sin(Radian) + RelativePosition.z * Mathf.Cos(Radian);
+
+        Vector3 result = new Vector3(x, y, z) + parent.transform.position;
+
+        return result;
     }
 }
 
